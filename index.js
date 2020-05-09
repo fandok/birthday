6e185aba2041c6b306020294ec5e6de4e041eeea
Fandika Okdiba <mr.parker0@gmail.com>
2020-05-09 22:16:03 +0700

    change route

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const app = express()
 const path = require('path')
 const port = 3000
 
-app.use(express.static(path.join(__dirname, 'public')))
+app.use('/', express.static(path.join(__dirname, 'public')))
 app.listen(port, () => console.log(`Listening on ${port}`))
