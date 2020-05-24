9ad1810163c932c44a8b0c0becc19affc00652ec
Fandika Okdiba <mr.parker0@gmail.com>
2020-05-24 23:03:00 +0700

    update text and color and show time

--- public/js/effect.js
+++ public/js/effect.js
@@ -204,13 +204,13 @@ $("document").ready(function () {
         function msgLoop(i) {
             $("p:nth-child(" + i + ")")
                 .fadeOut("slow")
-                .delay(800)
+                .delay(2000)
                 .promise()
                 .done(function () {
                     i = i + 1;
                     $("p:nth-child(" + i + ")")
                         .fadeIn("slow")
-                        .delay(1000);
+                        .delay(5000);
                     if (i == 50) {
                         $("p:nth-child(49)")
                             .fadeOut("slow")
